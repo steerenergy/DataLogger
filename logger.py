18e64948abdf51c7a313caad5ecaa57f72a1a559
Tom Gill <tom@steerenergy.com>
2018-02-22 16:33:55 +0000

    Updated code to simplify process for multiple units. Updated data rate to ensure correct pin is read. Created new file for adafruits simple test to reflect my config

--- logger.py
+++ logger.py
@@ -17,23 +17,32 @@ adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
 #  -  16 = +/-0.256V
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
-#Max Raw Data Value (15 bit)/Max voltage. Finds how many mV 1 bit represents.
+#Max Raw Data Value (15 bit)/Max voltage. Finds how many mV 1 bit represents. Note if gain is adjusted this will also need to be changed.
 voltageConvert = 4096.0/32767.0;
 
+time.sleep(2);
 ("Beginning Test...");
 while(True):
-    print("1st A/D Convertor:");
-    #Prints raw data from the A/D convertion straight orm the I2C Bus
-    raw = adc0.read_adc(3, gain=GAIN);
-    print("Raw Data:", raw);
-    #Converted to voltage using above conversion variable (voltageConvert)
-    voltage = (raw * voltageConvert);
-    print("Voltage:", round(voltage,2), "mV");
-    #Convert and print to temperature and line break
-    temp = (voltage-500)/10;
-    print("Temperature:", round(temp,2), "°C\n");
+    print("Start: \n");
+    for currentPin in range(4):
+        print("Current Pin: Pin A" + str(currentPin));
+        #Prints raw data from the A/D convertion , straight from the I2C Bus
+        raw = adc0.read_adc(currentPin, gain=GAIN, data_rate=860);
+        print("Raw Data:", raw);
+        #Converted to voltage using above conversion variable (voltageConvert)
+        voltage = (raw * voltageConvert);
+        print("Voltage:", round(voltage,2), "mV");
+        #Convert and print to temperature and line break
+        temp = (voltage-500)/10;
+        print("Temperature:", round(temp,2), "°C\n");
+        #split data to make easier to read and pause 2 seconds
+        print("-" *37);
+        time.sleep(2);
 
-    print("2nd A/D Convertor:");
+
+
+    
+    '''print("2nd A/D Convertor:");
     #Prints raw data from the A/D convertion straight orm the I2C Bus
     raw = adc1.read_adc(3, gain=GAIN);
     print("Raw Data:", raw);
@@ -45,5 +54,5 @@ while(True):
     print("Temperature:", round(temp,2), "°C\n");
     #split data to make easier to read and pause 2 seconds
     print("-" *37);
-    time.sleep(2);
+    time.sleep(2);'''
 
