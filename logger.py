432c509d3ade48bb58c8d95ddab2d89ce61b8082
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-02-28 15:38:11 +0000

    Fixed CSV Logging Test and initial RPI logging test

--- logger.py
+++ logger.py
@@ -2,6 +2,8 @@
 import time
 # Import the ADS1x15 module.
 import Adafruit_ADS1x15
+#Import CSV Logging Module
+import csv
 
 # Create an ADS1115 ADC (16-bit) instance.
 adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
@@ -24,29 +26,31 @@ voltageConvert = 4096.0/32767.0;
 currentAdc = adc0
 currentAdcText = "1st A/D Convertor";
 time.sleep(2);
-("Beginning Test...");
-while(True):
-    print("Reading Begin | Currently Selected:",currentAdcText);
-    print("-" *37,"\n");
-    time.sleep(0.5);
-    for currentPin in range(4):
-        print("Current Pin: Pin A" + str(currentPin));
-        #Prints raw data from the A/D convertion , straight from the I2C Bus
-        raw = currentAdc.read_adc(currentPin, gain=GAIN, data_rate=860);
-        print("Raw Data:", raw);
-        #Converted to voltage using above conversion variable (voltageConvert)
-        voltage = (raw * voltageConvert);
-        print("Voltage:", round(voltage,2), "mV");
-        #Convert and print to temperature and line break
-        temp = (voltage-500)/10;
-        print("Temperature:", round(temp,2), "°C\n");
-        #split data to make easier to read and pause 2 seconds
-        time.sleep(1);
-    #Select next A/D Convertor
-    if currentAdc == adc0:
-        currentAdc = adc1;
-        currentAdcText = "2nd A/D Convertor";
-    else:
-        currentAdc = adc0;
-        currentAdcText = "1st A/D Convertor";
-
+with open('TEST.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    ("Beginning Test...");
+    while(True):
+        print("Reading Begin | Currently Selected:",currentAdcText);
+        print("-" *37,"\n");
+        time.sleep(0.5);
+        for currentPin in range(4):
+            print("Current Pin: Pin A" + str(currentPin));
+            #Prints raw data from the A/D convertion , straight from the I2C Bus
+            raw = currentAdc.read_adc(currentPin, gain=GAIN, data_rate=860);
+            print("Raw Data:", raw);
+            #Converted to voltage using above conversion variable (voltageConvert)
+            voltage = (raw * voltageConvert);
+            print("Voltage:", round(voltage,2), "mV");
+            #Convert and print to temperature and line break
+            temp = (voltage-500)/10;
+            print("Temperature:", round(temp,2), "°C\n");
+            #split data to make easier to read and pause 2 seconds
+            writer.writerow(temp);
+            time.sleep(1);
+        #Select next A/D Convertor
+        if currentAdc == adc0:
+            currentAdc = adc1;
+            currentAdcText = "2nd A/D Convertor";
+        else:
+            currentAdc = adc0;
+            currentAdcText = "1st A/D Convertor";
