2ab336216daccdd8870b01ed9223b3e195ffe248
Tom Gill <tom@steerenergy.com>
2018-02-28 19:06:01 +0000

    Working Logging Copy

--- logger.py
+++ logger.py
@@ -1,13 +1,13 @@
 #Import Time for Delay functions etc
-import time
+import time;
 # Import the ADS1x15 module.
-import Adafruit_ADS1x15
+import Adafruit_ADS1x15;
 #Import CSV Logging Module
-import csv
+import csv;
 
 # Create an ADS1115 ADC (16-bit) instance.
-adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
-adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
+adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1);
+adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1);
 
 # Choose a gain of 1 for reading voltages from 0 to 4.09V.
 # Or pick a different gain to change the range of voltages that are read:
@@ -18,21 +18,25 @@ adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
 #  -   8 = +/-0.512V
 #  -  16 = +/-0.256V
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
-GAIN = 1
+GAIN = 1;
 #Max Raw Data Value (15 bit)/Max voltage. Finds how many mV 1 bit represents. Note if gain is adjusted this will also need to be changed.
 voltageConvert = 4096.0/32767.0;
 
+#set up list to be printed
+tempCSV = [0,0,0,0];
+
 #Chooses which A/D convertor is selected by default
-currentAdc = adc0
+currentAdc = adc0;
 currentAdcText = "1st A/D Convertor";
 time.sleep(2);
-with open('TEST.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+with open('data.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL);
     ("Beginning Test...");
+    writer.writerow(["A0","A1","A2","A3"]);
     while(True):
         print("Reading Begin | Currently Selected:",currentAdcText);
         print("-" *37,"\n");
-        time.sleep(0.5);
+        time.sleep(0.1);
         for currentPin in range(4):
             print("Current Pin: Pin A" + str(currentPin));
             #Prints raw data from the A/D convertion , straight from the I2C Bus
@@ -45,8 +49,13 @@ with open('TEST.csv', 'w', newline='') as csvfile:
             temp = (voltage-500)/10;
             print("Temperature:", round(temp,2), "°C\n");
             #split data to make easier to read and pause 2 seconds
-            writer.writerow(temp);
-            time.sleep(1);
+            tempCSV[currentPin] = temp;
+            time.sleep(0.01);
+        #Export Data to Spreadsheet and reset
+        writer.writerow(tempCSV);
+        print("CSV Values to Write:", tempCSV, "\n");
+        tempCSV = [0,0,0,0];
+        
         #Select next A/D Convertor
         if currentAdc == adc0:
             currentAdc = adc1;
