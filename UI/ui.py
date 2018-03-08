a6a520e091b85f06160421fa6ab6cb87814f62c6
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-08 09:03:06 +0000

    First Commit + Previous Test Data from end of Stage 4

--- UI/ui.py
+++ UI/ui.py
@@ -1,1 +1,6 @@
-print("Hello World")
+from paramiko.client import SSHClient
+
+client = SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(AutoAddPolicy())
+client.connect('192.168.1.106', username='pi', password='raspberry')
