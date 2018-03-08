6b7517900ac802d3a90eea83b039c8887a592b77
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-08 13:17:04 +0000

    Testing SSH and SFTP code for RPI communication

--- UI/ui.py
+++
@@ -1,6 +0,0 @@
-from paramiko.client import SSHClient
-
-client = SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(AutoAddPolicy())
-client.connect('192.168.1.106', username='pi', password='raspberry')
