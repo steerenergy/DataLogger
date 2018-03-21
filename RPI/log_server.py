2d91d101d93c7662e9a025bcd3b9bec21b082891
Tom Gill <tom@steerenergy.com>
2018-03-21 15:20:39 +0000

    Code Working RPI side for remote trigger part 1

--- RPI/log_server.py
+++ RPI/log_server.py
@@ -5,6 +5,8 @@ TCP_IP = '0.0.0.0'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
+print("Server Start \n")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
@@ -15,8 +17,9 @@ while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
     print("received data:", data.decode())
-    if data.decode() = "Read-Config"
+    if data.decode() == "Read-Config":
         print("Reading Config")
         conn.send("Reading Config File".encode())
+        conn.send("Time Interval is {} seconds".format("2").encode())
 
 conn.close()
