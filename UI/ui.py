040c08a5bd634e62189ca79cbd232be794791199
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-22 17:46:59 +0000

    Initial UI

---
+++ UI/ui.py
@@ -0,0 +1,11 @@
+print("Data Logger\n")
+print("-"*50,'\n')
+option = int(input("Main Menu \n Choose an option: \n 1.Party \n 2.Go Home \n 3.Cry About Life \n"))
+if option == 1:
+    print("Option 1")
+elif option == 2:
+    print("Option 2")
+elif option == 3:
+    print("Option 3")
+else:
+    print("Crey")
