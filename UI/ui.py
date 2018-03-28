4eaf641f4e92f71571a0f998b006300eab0eb1eb
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-28 09:03:38 +0100

    First UI

--- UI/ui.py
+++ UI/ui.py
@@ -1,11 +1,14 @@
 print("Data Logger\n")
 print("-"*50,'\n')
-option = int(input("Main Menu \n Choose an option: \n 1.Party \n 2.Go Home \n 3.Cry About Life \n"))
-if option == 1:
-    print("Option 1")
-elif option == 2:
-    print("Option 2")
-elif option == 3:
-    print("Option 3")
-else:
-    print("Crey")
+
+#Main Menu
+while True:
+option = int(input("Main Menu \n Choose an option: \n 1.Option 1 \n 2.Option 2 \n 3. Option 3 \n"))
+    if option == 1:
+        print("Option 1\n1")
+    elif option == 2:
+        print("Option 2\n")
+    elif option == 3:
+        print("Option 3\n")
+    else:
+        print("Invalid Option")
