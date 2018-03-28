51e057c59a1fc7a2992914fa2d78c37b6bbeafb5
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-28 10:33:02 +0100

    UI Polish

--- UI/ui.py
+++ UI/ui.py
@@ -1,30 +1,60 @@
 import sys
 class Menu:
+    #Top 3 are used consistantly in menu system
     def init(self):
-        print("Data Logger Pre Release")
-        print("-"*50,'\n')
+        print("Data Logger Pre Release Version Whatver")
+        print("-"*50,'')
         menu.main()
 
+    def quit(self):
+        print("\nGoodbye\n")
+        sys.exit()
+
+    def back(self):
+        raise StopIteration()
+
+    #Main Menu
     def main(self):
         #Main Menu
-        while True:
-            option = input("Main Menu: \nChoose a Option (based on the correspnding number): \n1. Logger Control\n2. Process Data \n3. General Settings \n4. About \n5. Quit \nOption: ")
-            #Set Menu Names
-            if option == "1":
-                menu.loggerControl()
-            elif option == "2":
-                menu.processData()
-            elif option == "3":
-                menu.generalSettings()
-            elif option == "4":
-                menu.about()
-            elif option == "5":
-                menu.quit()
-            else:
-                print("Invalid Option. Please Try Again\n")
+        try:
+            while True:
+                option = input("\nMain Menu: \nChoose a Option (based on the correspnding number): \n1. Logger Control\n2. Process Data \n3. General Settings \n4. About \n5. Quit \nOption Chosen: ")
+                #Set Menu Names
+                if option == "1":
+                    menu.loggerControl()
+                elif option == "2":
+                    menu.processData()
+                elif option == "3":
+                    menu.generalSettings()
+                elif option == "4":
+                    menu.about()
+                elif option == "5":
+                    menu.quit()
+                else:
+                    print("Invalid Option. Please Try Again\n")
+        except StopIteration:
+            pass
 
+    #Top Menu Comands
     def loggerControl(self):
-        print("\nOption 1\n")
+        try:
+            while True:
+                option = input("\nLogger Control Menu: \nChoose a Option (based on the correspnding number): \n1. Change Logger Config\n2. Download Data \n3. Realtime Data Output \n4. Back \n5. Quit \nOption Chosen: ")
+                #Set Menu Names
+                if option == "1":
+                    menu.loggerConfig()
+                elif option == "2":
+                    menu.downloadData()
+                elif option == "3":
+                    menu.realtimeData()
+                elif option == "4":
+                    menu.back()
+                elif option == "5":
+                    menu.quit()
+                else:
+                    print("Invalid Option. Please Try Again\n")
+        except StopIteration:
+            pass
 
     def processData(self):
         print("\nOption 2\n")
@@ -35,9 +65,13 @@ class Menu:
     def about(self):
         print("\nOption 4\n")
 
-    def quit(self):
-        print("\nGoodbye\n")
-        sys.exit()
+    #Logger Control Comands
+    def loggerConfig(self):
+        pass
+    def downloadData(self):
+        pass
+    def realtimeData(self):
+        print("\nComing Soon!\n")
 
 #Program Start
 menu = Menu()
