3241b0eec8a09f18502fc3ffeaadf4f3664cb9d6
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-03-28 10:07:04 +0100

    Working OOP menu

--- UI/ui.py
+++ UI/ui.py
@@ -1,14 +1,36 @@
-print("Data Logger\n")
-print("-"*50,'\n')
+import sys
+class Menu:
+    def init(self):
+        print("Data Logger Pre Release")
+        print("-"*50,'\n')
+        menu.main()
 
-#Main Menu
-while True:
-option = int(input("Main Menu \n Choose an option: \n 1.Option 1 \n 2.Option 2 \n 3. Option 3 \n"))
-    if option == 1:
-        print("Option 1\n1")
-    elif option == 2:
-        print("Option 2\n")
-    elif option == 3:
-        print("Option 3\n")
-    else:
-        print("Invalid Option")
+    def main(self):
+        #Main Menu
+        while True:
+            option = input("Main Menu: \nChoose a Option (based on the correspnding number): \n1. Option 1 \n2. Option 2 \n3. Option 3 \n4. Exit \nOption: ")
+            #Set Menu Names
+            if option == "1":
+                menu.option1()
+            elif option == "2":
+                menu.option2()
+            elif option == "3":
+                menu.option3()
+            elif option == "4":
+                menu.option4()
+            else:
+                print("Invalid Option. Please Try Again\n")
+
+    def option1(self):
+        print("\nOption 1\n")
+    def option2(self):
+        print("\nOption 2\n")
+    def option3(self):
+        print("\nOption 3\n")
+    def option4(self):
+        print("\nGoodbye\n")
+        sys.exit()
+
+#Program Start
+menu = Menu()
+menu.init()
