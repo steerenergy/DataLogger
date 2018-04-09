a14c7819b5f4b9babbe6bacac17dfcc27af3c94f
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-04-09 16:07:03 +0100

    Update LOgger for 4 channels

--- RPI/logger.py
+++ RPI/logger.py
@@ -12,31 +12,15 @@ dataRate=860
 #Max Raw Data Value (15 bit)/Max voltage. Finds how many mV 1 bit represents. Note if gain is adjusted this will also need to be changed.
 voltageConvert = 4096.0/32767.0
 #Set up list to be printed to CSV
-adcValues = [0]*16
+adcValues = [0]*4
 
 
-#A/D Setup - Create 4 instaces of  ADS1115 ADC (16-bit) according to Adafruit Libaries and then assign this to a big list
+#A/D Setup - Create instaces of ADS1115 ADC (16-bit) according to Adafruit Libaries and then assign this to a big list
 adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
-adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
-adc2 = Adafruit_ADS1x15.ADS1115(address=0x4a, busnum=1)
-adc3 = Adafruit_ADS1x15.ADS1115(address=0x4b, busnum=1)
-
 adcPinRead =[adc0.read_adc(0, gain=GAIN, data_rate=dataRate),\
 adc0.read_adc(1, gain=GAIN, data_rate=dataRate),\
 adc0.read_adc(2, gain=GAIN, data_rate=dataRate),\
-adc0.read_adc(3, gain=GAIN, data_rate=dataRate),\
-adc1.read_adc(0, gain=GAIN, data_rate=dataRate),\
-adc1.read_adc(1, gain=GAIN, data_rate=dataRate),\
-adc1.read_adc(2, gain=GAIN, data_rate=dataRate),\
-adc1.read_adc(3, gain=GAIN, data_rate=dataRate),\
-adc2.read_adc(0, gain=GAIN, data_rate=dataRate),\
-adc2.read_adc(1, gain=GAIN, data_rate=dataRate),\
-adc2.read_adc(2, gain=GAIN, data_rate=dataRate),\
-adc2.read_adc(3, gain=GAIN, data_rate=dataRate),\
-adc3.read_adc(0, gain=GAIN, data_rate=dataRate),\
-adc3.read_adc(1, gain=GAIN, data_rate=dataRate),\
-adc3.read_adc(2, gain=GAIN, data_rate=dataRate),\
-adc3.read_adc(3, gain=GAIN, data_rate=dataRate)]
+adc0.read_adc(3, gain=GAIN, data_rate=dataRate)]
 
 #Process Begin
 #First Line intro
@@ -49,7 +33,7 @@ try:
     #CSV setup
     with open('/home/pi/Github/DataLogger/RPI/voltage.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, dialect="excel", delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["Date/Time","Time Elapsed (Seconds)","0A0 (mV)","0A1 (mV)","0A2 (mV)","0A3 (mV)","1A0 (mV)","1A1 (mV)","1A2 (mV)","1A3 (mV)","2A0 (mV)","2A1 (mV)","2A2 (mV)","2A3 (mV)","3A0 (mV)","3A1 (mV)","3A2 (mV)","3A3 (mV)"])
+        writer.writerow(["Date/Time","Time Elapsed (Seconds)","0A0 (mV)","0A1 (mV)","0A2 (mV)","0A3 (mV)"])
 
         #Logging Process beginning
 
@@ -68,13 +52,13 @@ try:
             #Get time and send to Log
             currentDateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S %f");
             timeElapsed = round(time.perf_counter() - startTime,4)
-            for currentPin in range(16):
+            for currentPin in range(4):
                 #Get Raw data from A/D, convert to voltage and add to adcValues list corresponding to the current pin
                 adcValues[currentPin] = (adcPinRead[currentPin] * voltageConvert)
 
             #Export Data to Spreadsheet inc current datetime and time elasped and Reset list values (so we can see if code fails)
             writer.writerow([currentDateTime] + [timeElapsed] + adcValues)
-            adcValues = [0]*16
+            adcValues = [0]*4
             #Work out time delay needed until next set of values taken based on user given value (using some clever maths)
             timeDiff=(time.perf_counter() - startTime)
             time.sleep(timeInterval - (timeDiff % timeInterval))
