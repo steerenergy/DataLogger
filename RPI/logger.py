f8302bf94f4e51a0910e404b303bdeb1710224e4
Tom Gill <tom@steerenergy.com>
2018-04-10 11:33:12 +0100

    Update code in accordance with Hotfix-1

--- RPI/logger.py
+++ RPI/logger.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import Adafruit_ADS1x15
 import csv
+import functools
 #Variables
 
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
@@ -15,13 +16,17 @@ voltageConvert = 4096.0/32767.0
 adcValues = [0]*4
 
 
-#A/D Setup - Create instaces of ADS1115 ADC (16-bit) according to Adafruit Libaries and then assign this to a big list
+#A/D Setup - Create 4 instaces of  ADS1115 ADC (16-bit) according to Adafruit Libaries and then assign this to a big list
 adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
-adcPinRead =[adc0.read_adc(0, gain=GAIN, data_rate=dataRate),\
-adc0.read_adc(1, gain=GAIN, data_rate=dataRate),\
-adc0.read_adc(2, gain=GAIN, data_rate=dataRate),\
-adc0.read_adc(3, gain=GAIN, data_rate=dataRate)]
+adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
+adc2 = Adafruit_ADS1x15.ADS1115(address=0x4a, busnum=1)
+adc3 = Adafruit_ADS1x15.ADS1115(address=0x4b, busnum=1)
 
+
+adcPinRead =[functools.partial(adc0.read_adc,0, gain=GAIN, data_rate=dataRate),\
+functools.partial(adc0.read_adc,1, gain=GAIN, data_rate=dataRate),\
+functools.partial(adc0.read_adc,2, gain=GAIN, data_rate=dataRate),\
+functools.partial(adc0.read_adc,3, gain=GAIN, data_rate=dataRate)]
 #Process Begin
 #First Line intro
 print("Python Data Logger")
@@ -44,18 +49,21 @@ try:
             pass
 
         print("Logging Begin\n")
+
         #Set startTime (method used ignores changes in system clock time)
         startTime=time.perf_counter()
 
         #Beginning of reading script
         while(True):
             #Get time and send to Log
-            currentDateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S %f");
+            currentDateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f");
             timeElapsed = round(time.perf_counter() - startTime,4)
+
+            #REqrite using Enumerate (unpack the tuple)
             for currentPin in range(4):
                 #Get Raw data from A/D, convert to voltage and add to adcValues list corresponding to the current pin
-                adcValues[currentPin] = (adcPinRead[currentPin] * voltageConvert)
-            print(adcValues)
+                adcValues[currentPin] = (adcPinRead[currentPin]() * voltageConvert)
+            
             #Export Data to Spreadsheet inc current datetime and time elasped and Reset list values (so we can see if code fails)
             writer.writerow([currentDateTime] + [timeElapsed] + adcValues)
             adcValues = [0]*4
