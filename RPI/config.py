68f9fc722e705768fb6aa2b0b53b0c53d702a9d9
Tom Gill <tom@steerenergy.com>
2018-04-11 15:21:42 +0100

    Working Parsing of General and Pins and applying to objects

--- RPI/config.py
+++ RPI/config.py
@@ -2,6 +2,9 @@ import configparser
 
 
 class ADC:
+    pass
+   # def setup(self):
+        #if self.enabled =
 
 #Initial Import
 def init():
@@ -17,13 +20,32 @@ def generalImport():
     generalSettings = {}
     for key in config['General']:
         generalSettings[key] = config['General'][key]
+
     
 #Import Input Settings
 def inputImport():
-        for key in config['General']:
-        generalSettings[key] = config['General'][key]
-            for key in config['General']:
-                generalSettings[key] = config['General'][key]  
+    global adcList
+    adcList = {}
+    for input in config.sections():
+        if input != 'General':
+            adcList[input] = ADC()
+            for setting in config[input]:
+                adcList[input].enabled = config[input].getboolean('enabled')
+                adcList[input].inputType = config[input]['inputtype']
+                adcList[input].gain = config[input].getint('gain')
+                adcList[input].scaleLow = config[input].getint('scalelow')
+                adcList[input].scaleHigh = config[input].getint('scalehigh')
+                adcList[input].unit = config[input]['unit']
+    debugOutput()
+def debugOutput():
+    x = 0
+    for ADC in adcList:
+        x+=1
+        print("|{:>12}|{:>12}|{:>12}|{:>12}|{:>12}|{:>6}{:>6}|{:>12}|".format(x,ADC,adcList[ADC].enabled,adcList[ADC].inputType,adcList[ADC].gain,adcList[ADC].scaleLow,adcList[ADC].scaleHigh,adcList[ADC].unit))
+def inputProcess():
+    for ADC in adcList:
+        adcList[ADC].setup
+
 
 #Temp testing Code
 
