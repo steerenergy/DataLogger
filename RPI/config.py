a4339138c722970d07aff81496483362010beb7a
Tom Gill <tom@steerenergy.com>
2018-04-11 16:47:04 +0100

    First stages of interpreting imported data

--- RPI/config.py
+++ RPI/config.py
@@ -1,13 +1,45 @@
 import configparser
-
+import functools
+import Adafruit_ADS1x15
 
 class ADC:
-    pass
-   # def setup(self):
-        #if self.enabled =
+   def inputSetup(self):
+        if self.enabled == True:
+            adcToLog.append(self.name)
+        else:
+            pass
 
-#Initial Import
+#Initial Import and setup
 def init():
+    #A/D Setup - Create 4 instaces of  ADS1115 ADC (16-bit) according to Adafruit Libaries and then assign this to a big list
+    adc0 = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
+    adc1 = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
+    adc2 = Adafruit_ADS1x15.ADS1115(address=0x4a, busnum=1)
+    adc3 = Adafruit_ADS1x15.ADS1115(address=0x4b, busnum=1)
+
+    #Dictionary mapping input pins to physical devices
+    adcPinMap ={
+    "0A0": "functools.partial(adc0.read_adc,0, gain=GAIN, data_rate=dataRate)",
+    "0A1": "functools.partial(adc0.read_adc,1, gain=GAIN, data_rate=dataRate)",
+    "0A2": "functools.partial(adc0.read_adc,2, gain=GAIN, data_rate=dataRate)",
+    "0A3": "functools.partial(adc0.read_adc,3, gain=GAIN, data_rate=dataRate)",
+    "1A0": "functools.partial(adc1.read_adc,0, gain=GAIN, data_rate=dataRate)",
+    "1A1": "functools.partial(adc1.read_adc,1, gain=GAIN, data_rate=dataRate)",
+    "1A2": "functools.partial(adc1.read_adc,2, gain=GAIN, data_rate=dataRate)",
+    "1A3": "functools.partial(adc1.read_adc,3, gain=GAIN, data_rate=dataRate)",
+    "2A0": "functools.partial(adc2.read_adc,0, gain=GAIN, data_rate=dataRate)",
+    "2A1": "functools.partial(adc2.read_adc,1, gain=GAIN, data_rate=dataRate)",
+    "2A2": "functools.partial(adc2.read_adc,2, gain=GAIN, data_rate=dataRate)",
+    "2A3": "functools.partial(adc2.read_adc,3, gain=GAIN, data_rate=dataRate)",
+    "3A0": "functools.partial(adc3.read_adc,0, gain=GAIN, data_rate=dataRate)",
+    "3A1": "functools.partial(adc3.read_adc,1, gain=GAIN, data_rate=dataRate)",
+    "3A2": "functools.partial(adc3.read_adc,2, gain=GAIN, data_rate=dataRate)",
+    "3A3": "functools.partial(adc3.read_adc,3, gain=GAIN, data_rate=dataRate)"
+    }
+
+    
+    global adcToLog
+    adcToLog = []
     global config
     config = configparser.ConfigParser()
     config.read('logConf.ini')
@@ -30,21 +62,25 @@ def inputImport():
         if input != 'General':
             adcList[input] = ADC()
             for setting in config[input]:
+                adcList[input].name = input
                 adcList[input].enabled = config[input].getboolean('enabled')
                 adcList[input].inputType = config[input]['inputtype']
                 adcList[input].gain = config[input].getint('gain')
                 adcList[input].scaleLow = config[input].getint('scalelow')
                 adcList[input].scaleHigh = config[input].getint('scalehigh')
                 adcList[input].unit = config[input]['unit']
+            adcList[input].inputSetup()
     debugOutput()
+    print(adcToLog)
+    
 def debugOutput():
     x = 0
     for ADC in adcList:
         x+=1
-        print("|{:>12}|{:>12}|{:>12}|{:>12}|{:>12}|{:>6}{:>6}|{:>12}|".format(x,ADC,adcList[ADC].enabled,adcList[ADC].inputType,adcList[ADC].gain,adcList[ADC].scaleLow,adcList[ADC].scaleHigh,adcList[ADC].unit))
+        print("|{:>12}|{:>12}|{:>12}|{:>12}|{:>12}|{:>6}{:>6}|{:>12}|".format(x,adcList[ADC].name,adcList[ADC].enabled,adcList[ADC].inputType,adcList[ADC].gain,adcList[ADC].scaleLow,adcList[ADC].scaleHigh,adcList[ADC].unit))
 def inputProcess():
     for ADC in adcList:
-        adcList[ADC].setup
+        adcList[ADC].inputSetup
 
 
 #Temp testing Code
