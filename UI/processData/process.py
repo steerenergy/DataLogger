0b097104e5eba13c35f091d6fcc2d0c2e950299b
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-04-15 23:22:19 +0100

    Update Func to allow for different values

--- UI/processData/process.py
+++ UI/processData/process.py
@@ -4,20 +4,31 @@ import sys
 sys.path.append("..")
 import common
 
-def convert(value):
-    return value*2
-
 def init():
-    #Read read CSV
+    global conversion
+    conversion = {}
+    conversionSetup()
+    csvProcess()
+
+def conversionSetup():
+    conversion['0A1'] = 2
+    conversion['0A0'] = 3
+
+def csvProcess():
+    #read CSV
     df = pd.read_csv('raw.csv')
     print("Raw Data:")
     print(df)
     for item in df.iloc[:,2:].columns:
-        df[item] = df[item].apply(convert)
+        df[item] = df[item].apply(convert, args=(item,))
     print("\nConverted Data:")
     print(df)
     #Write Converted CSV
     df.to_csv('converted.csv', sep=',')
+
+def convert(value,item):
+    return value*conversion[item]
+
 
 if __name__ == "__main__":
     init()
