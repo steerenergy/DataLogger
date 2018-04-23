ac285fbaf68ff13cac46badb1b1474e3531074fd
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-04-23 14:17:31 +0100

    Menu Cleanup

--- UI/logCtrl/__init__.py
+++ UI/logCtrl/__init__.py
@@ -8,18 +8,16 @@ def init():
     try:
         while True:
             #Set Menu Names
-            option = input("\nLogger Control Menu: \nChoose a Option (based on the correspnding number): \n1. Control (Start/Stop) Logging \n2. Change Logger Config\n3. Download Data \n4. Realtime Data Output \n5. Back \n6. Quit \n\nOption Chosen: ")
+            option = input("\nLogger Control Menu: \nChoose a Option (based on the correspnding number): \n1. Control (Start/Stop) Logging\n2. Change Logger Config\n3. Realtime Data Output\n4. Back\n5. Quit\n\nOption Chosen: ")
             if option == "1":
                 control()
             elif option == "2":
                 config.init()
             elif option == "3":
-                downloadData()
-            elif option == "4":
                 realTime()
-            elif option == "5":
+            elif option == "4":
                 common.back()
-            elif option =="6":
+            elif option =="5":
                 common.quit()
             else:
                 common.other()
@@ -31,7 +29,6 @@ def init():
 #Logger Control Comands
 def control():
     print("\n Remote Control Coming Soon! \n Once the config is uploaded to the RPI, please start the program manually on the pi itself.")
-def downloadData():
-    pass
+#Future Realtime Data Code
 def realTime():
     print("\nComing Soon!\n")
