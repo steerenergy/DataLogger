17ebedd43fa64346d037d0813c66f0f1dbe61646
Tom Gill <tom@steerenergy.com>
2018-05-04 17:00:42 +0100

    End of Day Commit

--- RPI/gui.py
+++ RPI/gui.py
@@ -6,6 +6,7 @@ from tkinter import font
 import logger
 
 
+
 class Window(Frame):
     # Main Window
     def __init__(self, master=None):
@@ -13,26 +14,29 @@ class Window(Frame):
         Frame.__init__(self, master)
         # Setting self.master = master window
         self.master = master
-
+        
         # Changing the title of our master widget
         self.master.title("Data Logger")
         self.pack()
 
         # Title
-        self.title = Label(self, text="Data Logger", font=chosenFont)
+        self.title = Label(self, text="Data Logger", font=bigFont)
         self.title.pack()
 
         # Start/Stop Logging Button 
-        self.logButton = Button(self, text="Start Logging", height=4, width=20, command=self.test, font=chosenFont)
+        self.logButton = Button(self, text="Start Logging", height=4, width=20, command=self.test, font=bigFont)
         self.logButton.pack()
 
         # Start/Stop Logging Button 
-        self.quitButton = Button(self, text="Quit", height=4, width=20, command=self.client_exit, font=chosenFont)
+        self.quitButton = Button(self, text="Quit", height=4, width=20, command=self.client_exit, font=bigFont)
         self.quitButton.pack()
 
         # Live Data Title
-        self.liveTitle = Label(self, text="Data Logger", font=chosenFont)
+        self.liveTitle = Label(self, text="Live Data:", font=smallFont)
         self.liveTitle.pack()
+
+        #Live Data Table
+        #TBD
 
     def test(self):
         if self.logButton['text'] == "Start Logging":
@@ -62,7 +66,9 @@ root = Tk()
 # Size of the window
 # root.wm_attributes('-zoomed', 1)
 
-chosenFont = font.Font(family="Helvetica", size=20, weight=font.BOLD)
+# Fonts
+bigFont = font.Font(family="Helvetica", size=20, weight=font.BOLD)
+smallFont = font.Font(family="Helvetica", size=14)
 
 app = Window(root)
 root.mainloop()
