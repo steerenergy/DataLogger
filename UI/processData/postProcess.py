cc33566d6cbb2dc096620c2e3eaf4dbbc14ed8e5
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-05-29 16:59:33 +0100

    Created framework for postprocess and converting time columns into time + making timestamp compliant

--- UI/processData/postProcess.py
+++ UI/processData/postProcess.py
@@ -6,18 +6,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Contains all the functions for processing data and loading/exporting the CSV file
 class Process:
     def __init__(self):
         # This is a temporary file path. A file selection system will need to be implemented (See X01 doc)
-        self.csvFilePath = "files/converted/raw20180525120257197352.csv"
+        self.csvDirectory = "files/converted"
+        self.convertedCsvFile = "converted20180525120257197320.csv"
+        self.convertedCsvFilePath = self.csvDirectory + "/" + self.convertedCsvFile
+        self.processedCsvFile = self.convertedCsvFile.replace("converted", "processed")
+        self.processedCsvFilePath = self.csvDirectory + "/" + self.processedCsvFile
+        # Pandas Dataframe
         self.df = None
         # Trigger Pandas Init
         self.pandasInit()
 
     def pandasInit(self):
-        self.df = pd.read_csv(self.csvFilePath)
-        print("\nCurrent Data:")
-        print(self.df.head)
+        # Load in CSV and print CSV contents
+        self.df = pd.read_csv(self.convertedCsvFilePath)
+        print("\nCurrent Data Preview:")
+        print(self.df.head())
+        # Converting the First Column to DateTime (Used for Compression)
+        self.df.iloc[:, 0] = pd.to_datetime(self.df.iloc[:, 0])
+        # Converting the Second Column to DateTime (Used for Compression)
+        self.df.iloc[:, 1] = pd.to_datetime(self.df.iloc[:, 1])
+        print(self.df)
+        print("\n" + str(self.df.dtypes))
 
     # Filter Functions
     def filter(self):
@@ -25,11 +38,18 @@ class Process:
 
     # Compress Functions
     def compress(self):
-        pass
+        print(self.df[1].resample('5Min'))
 
     # Plot Functions
     def plot(self):
         pass
+
+    # Write Updated CSV File
+    def pandasExit(self):
+        print("\nWriting CSV...")
+        # Write CSV
+        self.df.to_csv(self.processedCsvFilePath, sep=',', index=False)
+        print("\nSuccess")
 
 
 # Main Menu
@@ -39,8 +59,8 @@ def init():
     try:
         while True:
             option = input("\nPost Process Menu: \nChoose a Option (based on the corresponding number): "
-                           "\n1. Filter\n2. Compress\n3. Plot"
-                           "\n4. Back\n5. Quit \n\nOption Chosen: ")
+                           "\n1. Filter\n2. Compress\n3. Plot\n4. Save File"
+                           "\n----------------\n5. Back\n6. Quit \n\nOption Chosen: ")
             if option == "1":
                 data.filter()
             elif option == "2":
@@ -48,8 +68,10 @@ def init():
             elif option == "3":
                 data.plot()
             elif option == "4":
-                common.back()
+                data.pandasExit()
             elif option == "5":
+                common.back()
+            elif option == "6":
                 common.quit()
             else:
                 common.other()
