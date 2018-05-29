553c3b2dde0b8cbb76e6e973102a7ce57b3201bb
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-05-29 13:48:12 +0100

    Final commit before Tom gets of Nicks' Branch

--- UI/processData/postProcess.py
+++ UI/processData/postProcess.py
@@ -5,22 +5,48 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# This is a temporary file path. A file selection system will need to be implemented (See X01 doc)
-csvFilePath = "/files/converted/raw20180525120257197352.csv"
+
+class Process:
+    def __init__(self):
+        # This is a temporary file path. A file selection system will need to be implemented (See X01 doc)
+        self.csvFilePath = "files/converted/raw20180525120257197352.csv"
+        self.df = None
+        # Trigger Pandas Init
+        self.pandasInit()
+
+    def pandasInit(self):
+        self.df = pd.read_csv(self.csvFilePath)
+        print("\nCurrent Data:")
+        print(self.df.head)
+
+    # Filter Functions
+    def filter(self):
+        pass
+
+    # Compress Functions
+    def compress(self):
+        pass
+
+    # Plot Functions
+    def plot(self):
+        pass
+
 
 # Main Menu
 def init():
+    # Create instance of process class
+    data = Process()
     try:
         while True:
             option = input("\nPost Process Menu: \nChoose a Option (based on the corresponding number): "
                            "\n1. Filter\n2. Compress\n3. Plot"
                            "\n4. Back\n5. Quit \n\nOption Chosen: ")
             if option == "1":
-                dataFilter()
+                data.filter()
             elif option == "2":
-                dataCompress()
+                data.compress()
             elif option == "3":
-                dataPlot()
+                data.plot()
             elif option == "4":
                 common.back()
             elif option == "5":
@@ -29,18 +55,3 @@ def init():
                 common.other()
     except StopIteration:
         pass
-
-
-# Filter Functions
-def dataFilter():
-    pass
-
-
-# Compress Functions
-def dataCompress():
-    pass
-
-
-# Plot Functions
-def dataPlot():
-    pass
