87ed7bad69e3ac06214f5d6702a57698bbaaf61b
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-06-05 14:22:14 +0100

    Updated UI back to original code

--- UI/ui.py
+++ UI/ui.py
@@ -1,13 +1,7 @@
 # MAIN PROGRAM - Start from here
 
-from logCtrl import config
-config.init()
-
-'''
 # Import local python files for operation
 import menu
 
 # Initiate Main Menu
-menu.init() 
-
-'''
+menu.init()
