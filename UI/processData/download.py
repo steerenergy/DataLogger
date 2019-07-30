cf0a225865eef3c7be6db3f9a39a43e9437849fc
steerenergy <36624165+steerenergy@users.noreply.github.com>
2019-07-30 11:32:20 +0100

    Format Correction

--- UI/processData/download.py
+++ UI/processData/download.py
@@ -50,7 +50,7 @@ def init():
                     # Delete File if user wants to
                     # This means file is not deleted if the file is taken off during logging
                     option = input("Delete File Remotely?"
-                                   "IMPORTANT: Do not delete files during logger operation!\n(Y/N): ")
+                                   " - IMPORTANT: Do not delete files during logger operation!\n(Y/N): ")
                     if option == "Y" or option == "y":
                         sftp.remove(remotePath)
                         print("Successfully Deleted: {}".format(fileName))
