5824fb3098767a9a39fea4105d442956e7359a91
tomgillsteer <tom@steerenergy.com>
2019-08-02 11:42:57 +0100

    Further Error Logging Tests

--- RPI/test.py
+++ RPI/test.py
@@ -1,3 +1,29 @@
+""" import logging
 import sys
-sys.stderr = open('error.txt', 'w')
-sys.stderr.write("THIS IS AN ERROR")
+
+logger = logging.getLogger('error_logger')
+def exception_handler(exc_type, exc_value, exc_traceBack):
+    logger.exception("Unhandled Exception! - Type: {}\n Value: {}\n Traceback: {}".format(exc_type, exc_value, exc_traceBack))
+
+# Redirect Exceptions to text file
+sys.excepthook = exception_handler
+
+print("Running")
+raise RuntimeError """
+
+
+import logging
+import sys
+
+logger = logging.getLogger('mylogger')
+# Configure logger to write to a file...
+
+def my_handler(type, value, tb):
+    logger.exception("Uncaught exception: {0}".format(str(value)))
+
+# Install exception handler
+sys.excepthook = my_handler
+
+# Run your main script here:
+if __name__ == '__main__':
+    main()
