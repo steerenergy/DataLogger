64c81a10466e79993cd4b419b991a32b33e80239
tomgillsteer <tom@steerenergy.com>
2019-08-19 11:35:06 +0100

    Complete Test File for handeling exceptions

--- RPI/test.py
+++ RPI/test.py
@@ -1,21 +1,27 @@
 import logging
 import sys
+from datetime import datetime
 
 # Setup error logging
-logger = logging.getLogger('error_logger')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('error.log')
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
+# Note exceptions will print to console when running from Thonny IDE! To test, run script from GUI
+def errorLoggingSetup():
+    errorLogger = logging.getLogger('error_logger')
+    errorLogger.setLevel(logging.INFO)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('error.log')
+    fh.setLevel(logging.INFO)
+    errorLogger.addHandler(fh)
+    # Print Top Line to make it easy to identify new instance of program
+    errorLogger.info("\n{}\nNEW INSTANCE OF LOGGER @ {}\n{}\n".format('-'*75, datetime.now(), '-'*75))
+
+# Start Logging
+errorLoggingSetup()
 
 
-def exception_handler(exc_type, exc_value, exc_traceBack):
-    print("UNANDELED EXCEPTION - Check Log File")
-    logger.error("Unhandled Exception!\nType: {}\nValue: {}\nTraceback: {}\n".format(exc_type, exc_value, exc_traceBack))
-
-# Redirect Exceptions to text file
-sys.excepthook = exception_handler
-
-print("Running")
-raise RuntimeError
+try: 
+    print("Running")
+    print(dfdkhk)
+except Exception:
+    errorLogger = logging.getLogger('error_logger')
+    print("UNHANDLED EXCEPTION - Check Log File")
+    errorLogger.exception("Unhandeled Exception! \nTime/Date: {}\nDetails:\n".format(datetime.now()))
