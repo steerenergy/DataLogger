44bb57ff33b41568c21023722544949ed3ee8f74
steerenergy <36624165+steerenergy@users.noreply.github.com>
2019-08-22 14:59:00 +0100

    Update download.py
    Commenting Improved
    Number of files found now displayed
    Improve Transferring Percentage Display
    Asked if all files want to be deleted rather than just some of them

--- UI/processData/download.py
+++ UI/processData/download.py
@@ -31,39 +31,55 @@ def init():
         print("Getting Config(s) and CSV(s)...")
         sftp = paramiko.SFTPClient.from_transport(transport)
 
+        # Get List of Files
+        filesList = sftp.listdir(path='/home/pi/Github/DataLogger/RPI/files/outbox')
+
         # Give warning if there are no files to download
-        if len(sftp.listdir(path='/home/pi/Github/DataLogger/RPI/files/outbox')) <= 1:
+        if len(filesList) <= 1:
             print("No Files found to Download")
         else:
+            # Print Number of Files Found
+            noFiles = "\n{} Files Found".format(len(filesList))
+            print(noFiles)
+            # Make it look pretty
+            print("-"*(len(noFiles)-1))
             # Download all files in remote outbox folder and delete them after
-            for fileName in sftp.listdir(path='/home/pi/Github/DataLogger/RPI/files/outbox'):
+            for fileName in filesList:
+                # Don't download .gitkeep file or bad things happen
                 if fileName != ".gitkeep":
                     # Download File
                     remotePath = '/home/pi/Github/DataLogger/RPI/files/outbox/' + fileName
-                    localPath = 'files/inbox/'+fileName
+                    localPath = 'files/inbox/' + fileName
                     # Print name of file being transferred
-                    print("\nTransferring: {}...".format(fileName))
+                    print("File: {}:".format(fileName))
                     # Transfer the file and give status of transfer (see printTotals func at top).
                     sftp.get(remotePath, localPath, callback=printTotals)
-                    # Print Success
-                    print("Successfully Downloaded: {}".format(fileName))
-                    # Delete File if user wants to
-                    # This means file is not deleted if the file is taken off during logging
-                    option = input("Delete File Remotely?"
-                                   " - IMPORTANT: Do not delete files during logger operation!\n(Y/N): ")
-                    if option == "Y" or option == "y":
+                    # Print Success (note this is printed on the line above
+                    # due to carriage return on the above statement
+                    print("Transferred: 100% - Success")
+
+            # Give option for user to delete all files on Pi
+            # This means files are not deleted if the file is taken off during logging
+            option = input("Delete Files On Pi?"
+                           " - IMPORTANT: Do not delete files during logger operation!\n(Y/N): ")
+            if option == "Y" or option == "y":
+                for fileName in filesList:
+                    # Don't download .gitkeep file or bad things happen
+                    if fileName != ".gitkeep":
+                        # Get remote path and delete
+                        remotePath = '/home/pi/Github/DataLogger/RPI/files/outbox/' + fileName
                         sftp.remove(remotePath)
-                        print("Successfully Deleted: {}".format(fileName))
-                    elif option == "N" or option == "n":
-                        print("File Not Deleted")
-                    else:
-                        print("Invalid Option - File Not Deleted")
+                print("Successfully Deleted Files on Pi")
+            elif option == "N" or option == "n":
+                print("Files Not Deleted")
+            else:
+                print("Invalid Option - Files Not Deleted")
 
         # Close Connection
         sftp.close()
         transport.close()
         # Print Success
-        print("Success!")
+        print("\nSuccess!")
 
     # If connection was unsuccessful
     except socket.error:
